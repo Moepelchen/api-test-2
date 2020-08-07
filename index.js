87900f301ec7f492d4ac552b9d98ff283460e752
Ulrich Tewes <u.tewes@comventure.de>
2020-08-07 16:09:23 +0200

    finally working

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require("body-parser");
+var app = express();
+var port = process.env.PORT || 8888;
+console.log(process.env.PORT);
+app.use(bodyParser.json())
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
+app.get("/okta", (req, res, next) => {
+    res.json({ "verification" : req.headers["x-okta-verification-challenge"] });
+});
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get("/", (req, res, next) => {
+    res.json({ "hello" : "world"});
+});
+
+app.post("/okta", (req, res, next) => {
+    console.dir(req.body);
+    res.json({ "hello" : req.body});
+});
