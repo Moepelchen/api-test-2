9e4a425fbf5f7b1b49c09de61f868e100b20b390
Ulrich Tewes <u.tewes@comventure.de>
2020-08-07 16:29:04 +0200

    finally working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 8888;
 console.log(process.env.PORT);
-app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.json({extended: false}))
 app.listen(port, () => {
     console.log("Server running on port " + port);
 });
@@ -16,6 +16,6 @@ app.get("/", (req, res, next) => {
 });
 
 app.post("/okta", (req, res, next) => {
-    console.log(req.body);
-    res.json({ "hello" : req.body});
+    console.log(req.body.data.events);
+    res.json({ "hello" : req.body.data.events});
 });
